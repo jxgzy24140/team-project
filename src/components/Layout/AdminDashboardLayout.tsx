c70dd81ef5ea4a6b35671515c830ab92647f0f30
hakiet <kiet24140@gmail.com>
2024-02-21 15:24:29 +0700

    admin layout

--- src/components/Layout/AdminDashboardLayout.tsx
+++ src/components/Layout/AdminDashboardLayout.tsx
@@ -1,34 +1,29 @@
 import React, { Suspense } from "react";
-import { Layout } from "antd";
-import { Route, Routes } from "react-router-dom";
-import { appLayout } from "@/components/Layout/Router/router.config";
-import ProtectedRoute from "@/components/Layout/Router/ProtectedRoute";
+import { Layout, Row, Col } from "antd";
 import { AdminHeaderLayout } from "@/components/Layout/Header";
+import Sidebar from "@/components/Layout/Sidebar";
 const { Content } = Layout;
 const AdminDashboardLayout = () => {
   return (
-    <Layout className="w-full h-full min-h-screen overflow-y-scroll">
-      <AdminHeaderLayout />
-      <Content className="h-full min-h-full" style={{ minHeight: "unset" }}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            {appLayout.map((route: any, key: any) => {
-              const Component = route.component;
-              return (
-                <Route
-                  key={key}
-                  path={route.path}
-                  element={
-                    <ProtectedRoute>
-                      <Component />
-                    </ProtectedRoute>
-                  }
-                />
-              );
-            })}
-          </Routes>
-        </Suspense>
-      </Content>
+    <Layout className="w-full h-screen max-h-screen flex flex-row ">
+      <Col span={4} style={{ border: "1px solid red" }}>
+        <Sidebar />
+      </Col>
+      <Col span={20} style={{ border: "1px solid blue" }}>
+        <Row className="flex-col">
+          <Col className="h-10" style={{ border: "1px solid yellow" }}>
+            <AdminHeaderLayout />
+          </Col>
+          <Col style={{ height: "calc(100vh - 40px)" }}>
+            <Content
+              className="h-full min-h-full bg-slate-500"
+              style={{ minHeight: "unset" }}
+            >
+              <Suspense fallback={<div>Loading...</div>}></Suspense>
+            </Content>
+          </Col>
+        </Row>
+      </Col>
     </Layout>
   );
 };
--- src/components/Layout/Header/AdminHeaderLayout.tsx
+++ src/components/Layout/Header/AdminHeaderLayout.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Col, Form, Button, Input } from "antd";
 
 export const AdminHeaderLayout = () => {
   return (
-    <Layout.Header className="bg-white opacity-85 container min-w-full"></Layout.Header>
+    <Layout.Header className="bg-white opacity-85 container w-full min-w-full flex flex-row">
+      <Col span={12}>
+        <Form className="flex">
+          <Input placeholder="Type something..." />
+          <Button htmlType="submit">Search</Button>
+        </Form>
+      </Col>
+      <Col span={12}>User</Col>
+    </Layout.Header>
   );
 };
